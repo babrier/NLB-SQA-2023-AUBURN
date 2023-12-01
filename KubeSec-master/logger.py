ca9407ad4d34018e3d7ed920032b1c8a11ff37bf
liamhussey <lph0017@auburn.edu>
2023-12-01 16:09:58 -0600

    Bug fixes for logger

--- KubeSec-master/logger.py
+++ KubeSec-master/logger.py
@@ -5,9 +5,9 @@ def create_logger(script_name):
 	logger = logging.getLogger('logs')
 	logger.setLevel(logging.DEBUG)
 
-	fileName = f"./logs/{script_name}_on_{datetime.now().strftime('%m-%d-%Y@%H:%M:%S')}.log"
+	fileName = f"./KubeSec-master/logs/{script_name}_on_{datetime.now().strftime('%m-%d-%Y@%H:%M:%S')}.log"
 
-	fileName_Readme = f"./logs/README.md"
+	fileName_Readme = f"./KubeSec-master/logs/README.md"
 
 	handler = logging.FileHandler(fileName_Readme, mode = 'w')
 	formatter = logging.Formatter('%(asctime)s | %(levelname)s | %(message)s')
